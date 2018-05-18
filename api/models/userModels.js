e36aeb1b7c3f0760801ef0c712932cb1dfa253d8
unknown <sycode@gmail.com>
2018-05-18 10:46:21 -0600

    userSchema & userModel completed

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -4,24 +4,33 @@ const Schema = mongoose.Schema;
 
 const SALT_ROUNDS = 11;
 
-const UserSchema = Schema({
-  // create your user schema here.
-  // username: required, unique and lowercase
-  // password: required
+const UserSchema = mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
-UserSchema.pre('save', function(next) {
-  // https://github.com/kelektiv/node.bcrypt.js#usage
-  // Fill this middleware in with the Proper password encrypting, bcrypt.hash()
-  // if there is an error here you'll need to handle it by calling next(err);
-  // Once the password is encrypted, call next() so that your userController and create a user
+UserSchema.pre('save', function(next){
+  bcrypt.hash(this.password, SALT_ROUNDS, (err, hash) => {
+      if(err) {
+          return next(err);
+      }
+      this.password = hash;
+      return next();
+  });
 });
 
-UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
-  // https://github.com/kelektiv/node.bcrypt.js#usage
-  // Fill this method in with the Proper password comparing, bcrypt.compare()
-  // Your controller will be responsible for sending the information here for password comparison
-  // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
+UserSchema.methods.checkPassword = function(passwordGuess, cb) {
+  bcrypt.compare(passwordGuess, this.password, (err, isMatch) => {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
 };
 
 module.exports = mongoose.model('User', UserSchema);
--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const port = process.env.PORT || 8000;
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/auth-users', {
   useMongoClient: true
-});
+}).then(conn => {
+  console.log('\n connected to localhost mongodb auth-users \n');
+})
+.catch(err => console.log('error connecting to mongo', err));;
 
 server.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  console.log(`\n Server listening on port ${port}`);
 });
